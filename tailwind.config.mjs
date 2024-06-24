347bb9e442c666cb4a9a5a2ada569fbdaeffc0f0
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-06-24 09:03:43 -0500

    navBar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        greenOne: "#7CB518",
+        greenTwo: "#5C8001",
+        orangeOne: "#FBB02D",
+        orangeTwo: "#fb6107",
+      },
+    },
+  },
+  plugins: [],
+};
